1b741175ab0c01aecf6c04f03b1ff80548872e2d
DanHenrique <daanielhenrique7@gmail.com>
2024-06-29 13:39:54 -0300

    infra: terraform

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,15 +1,20 @@
 module "lambda" {
-  source               = "github.com/DanHenrique/terraform-aws-lambda?ref=v0.0.1"
+  source               = "github.com/DanHenrique/terraform-aws-lambda?ref=v0.0.2"
 
   # Lambda configuration
-  function_name         = local.function_name
-  function_handler      = local.function_handler
-  function_runtime      = local.function_runtime
-  function_role         = local.function_role
-  lambda_code_path      = local.lambda_code_path
+  function_runtime = local.function_runtime
+  function_code = local.lambda_code_path
+  function_name            = local.function_name
+  function_description = "Lambda responsible for validating JSON schemas"
+  function_handler = local.function_handler
+
+  function_role = local.function_role
   environment_variables = {}
   # CloudWatch configuration
-  log_retention_days    = local.log_retention_days
+  log_retention_days = 1
 
-  tags                  = local.tags
+  tags = {
+    creator        = "danhenrique"
+    git_repository = "https://github.com/DanHenrique/lambda-schema-validator"
+  }
 }
--- infra/locals.tf
+++ infra/locals.tf
@@ -5,8 +5,8 @@ locals {
     # Lambda configuration
     function_name     = "util-schema-validator"
     function_handler  = "lambda_function.lambda_handler"
-    function_runtime  = "python3.8"
-    lambda_code_path  = "../app/src"
+    function_runtime  = "python3.11"
+    lambda_code_path  = "../app/dist/app.zip"
     function_role     = module.iam_role_with_policies.role_arn
     # CloudWatch configuration
     log_retention_days = 1
