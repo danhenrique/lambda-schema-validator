c5b713a3ebf1453fd7dc441f2bd33e8a776ed99f
DanHenrique <daanielhenrique7@gmail.com>
2024-06-30 01:20:39 -0300

    fix: tests

--- app/src/validation/__init__.py
+++ app/src/validation/__init__.py
@@ -5,17 +5,17 @@ __author__ = 'DanHenrique'
 
 __all__ = []
 
-import importlib
 from jsonschema import validate as validate_schema
-from . import schemas_definitions as defs
-
-schemas = defs.schemas
+# Schemas definitions
+from .schemas import person
+from .schemas import product
 
 
 def _get_schema(n):
-    if n in schemas:
-        module = importlib.import_module(schemas[n.lower()], __name__)
-        return module.schema
+    if n == 'person':
+        return person.schema
+    elif n == 'product':
+        return product.schema
     else:
         raise ValueError(f"Schema {n} not found.")
 
