64fbc27563ce9a18418cf58d74f961eb68714bca
DanHenrique <daanielhenrique7@gmail.com>
2024-06-30 02:00:44 -0300

    corrigindo formatação

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,14 +1,14 @@
 module "lambda" {
-  source               = "github.com/DanHenrique/terraform-aws-lambda?ref=v0.0.2"
+  source = "github.com/DanHenrique/terraform-aws-lambda?ref=v0.0.2"
 
   # Lambda configuration
-  function_runtime = local.function_runtime
-  function_code = local.lambda_code_path
-  function_name            = local.function_name
+  function_runtime     = local.function_runtime
+  function_code        = local.lambda_code_path
+  function_name        = local.function_name
   function_description = "Lambda responsible for validating JSON schemas"
-  function_handler = local.function_handler
+  function_handler     = local.function_handler
 
-  function_role = local.function_role
+  function_role         = local.function_role
   environment_variables = {}
   # CloudWatch configuration
   log_retention_days = 1
